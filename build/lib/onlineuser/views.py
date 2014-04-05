b7fd2554ba3bb58337d7d71af929f02aeae96da9
Diogo Lima <diogo@dr6.com.br>
2014-04-05 19:09:58 -0300

    cleanup

--- build/lib/onlineuser/views.py
+++
@@ -1,9 +0,0 @@
-import json
-from django.http import HttpResponse
-from django.shortcuts import render
-from onlineuser.models import getOnlineInfos
-
-def get_online_users(request):
-	res = getOnlineInfos(detail=True, json=True)
-	response = json.dumps(res, ensure_ascii=False)
-	return HttpResponse(response, content_type="application/json")
